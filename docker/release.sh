e57a9bf20edb8c9d7575b44ff9722d7b682776c8
应程鹏 <ychp@terminus.io>
2018-07-25 21:19:27 +0800

    update port

--- docker/release.sh
+++ docker/release.sh
@@ -5,4 +5,4 @@ mvn clean package -Dmaven.test.skip
 cp /root/workspace/blog/web/target/blog.jar /root/workspace/blog/docker/
 cd /root/workspace/blog/docker
 docker build . -t blog:1.0
-docker run -p 5201:5201 -v /var/log/blog:/var/log/blog -dit blog:1.0
+docker run -p 520:520 -v /var/log/blog:/var/log/blog -dit blog:1.0
