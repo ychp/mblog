8291d2a056605e6f2f0d3214ec11d7019690bc47
应程鹏 <ychp@terminus.io>
2018-08-08 10:33:48 +0800

    update docker file

--- release.sh
+++ release.sh
@@ -31,15 +31,15 @@ else
     echo "请输入redis密码:"
     read redis_password
 
-    echo 'docker run -p 8099:8099
-        -v /var/log/blog:/var/log/blog
-        -e MYSQL_HOST='${mysql_host}'
-        -e MYSQL_PORT='${mysql_port}'
-        -e MYSQL_DATABASE='${mysql_database}'
-        -e MYSQL_PASSWORD='${mysql_password}'
-        -e REDIS_HOST='${redis_host}'
-        -e REDIS_PORT='${redis_port}'
-        -e REDIS_AUTH='${redis_password}'
+    echo 'docker run -p 8099:8099 \
+        -v /var/log/blog_new:/var/log/blog \
+        -e MYSQL_HOST='${mysql_host}' \
+        -e MYSQL_PORT='${mysql_port}' \
+        -e MYSQL_DATABASE='${mysql_database}' \
+        -e MYSQL_PASSWORD='${mysql_password}' \
+        -e REDIS_HOST='${redis_host}' \
+        -e REDIS_PORT='${redis_port}' \
+        -e REDIS_AUTH='${redis_password}' \
         -dit blog:1.0' >> ~/blog.sh
     firewall-cmd --permanent --add-port=8099/tcp
     firewall-cmd --reload
