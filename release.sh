b92724d11cff5ef004b27bbb34a9afceb406f441
应程鹏 <ychp@terminus.io>
2018-08-08 09:37:44 +0800

    update script

--- release.sh
+++ release.sh
@@ -1,8 +1,8 @@
-#bash
+#!/usr/bin/env bash
 set -e
-cd /root/workspace/blog
+local_path=`pwd`
 mvn clean package -Dmaven.test.skip
-cp /root/workspace/blog/web/target/blog.jar /root/workspace/blog/docker/
-cd /root/workspace/blog/docker
+cp ${local_path}/web/target/blog.jar ${local_path}/docker/
+cd ${local_path}/docker
 docker build . -t blog:1.0
 docker run -p 520:520 -v /var/log/blog:/var/log/blog -dit blog:1.0
