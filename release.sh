c4eedb0609550eab6674599adfa17bef94cdc54a
应程鹏 <ychp@terminus.io>
2018-08-11 19:53:35 +0800

    update shell

--- release.sh
+++ release.sh
@@ -25,7 +25,7 @@ else
     echo '  docker rm $pid' >> ~/blog.sh
     echo '  rm /var/run/blog-api.pid' >> ~/blog.sh
     echo 'fi;' >> ~/blog.sh
-    echo 'pid=`docker run -p 8099:8099 -v /var/log/blog_new:/var/log/blog \'
+    echo 'pid=`docker run -p 8099:8099 -v /var/log/blog_new:/var/log/blog \' >> ~/blog.sh
 
     echo "请输入数据库连接:"
     read mysql_host
@@ -39,10 +39,10 @@ else
     echo "请输入数据库密码:"
     read mysql_password
 
-    echo '-e MYSQL_HOST='${mysql_host}' \'
-    echo '-e MYSQL_PORT='${mysql_port}' \'
-    echo '-e MYSQL_DATABASE='${mysql_database}' \'
-    echo '-e MYSQL_PASSWORD='${mysql_password}' \'
+    echo '-e MYSQL_HOST='${mysql_host}' \' >> ~/blog.sh
+    echo '-e MYSQL_PORT='${mysql_port}' \' >> ~/blog.sh
+    echo '-e MYSQL_DATABASE='${mysql_database}' \' >> ~/blog.sh
+    echo '-e MYSQL_PASSWORD='${mysql_password}' \' >> ~/blog.sh
 
     echo "请输入redis链接:"
     read redis_host
@@ -53,35 +53,35 @@ else
     echo "请输入redis密码:"
     read redis_password
 
-    echo '-e REDIS_HOST='${redis_host}' \'
-    echo '-e REDIS_PORT='${redis_port}' \'
-    echo '-e REDIS_AUTH='${redis_password}' \'
+    echo '-e REDIS_HOST='${redis_host}' \' >> ~/blog.sh
+    echo '-e REDIS_PORT='${redis_port}' \' >> ~/blog.sh
+    echo '-e REDIS_AUTH='${redis_password}' \' >> ~/blog.sh
 
     echo "请输入对象存储服务类型:"
     read file_type
-    echo '-e FILE_TYPE='${file_type}' \'
+    echo '-e FILE_TYPE='${file_type}' \' >> ~/blog.sh
 
     if [ "$file_type" == "cos" ];
     then
         echo "请输入COS secretId:"
         read cos_secret_id
-        echo '-e COS_SECRET_ID='${cos_secret_id}' \'
+        echo '-e COS_SECRET_ID='${cos_secret_id}' \' >> ~/blog.sh
 
         echo "请输入COS secretKey:"
         read cos_secret_key
-        echo '-e COS_SECRET_KEY='${cos_secret_key}' \'
+        echo '-e COS_SECRET_KEY='${cos_secret_key}' \' >> ~/blog.sh
 
         echo "请输入COS appId:"
         read cos_app_id
-        echo '-e COS_APP_ID='${cos_app_id}' \'
+        echo '-e COS_APP_ID='${cos_app_id}' \' >> ~/blog.sh
 
         echo "请输入COS bucketName:"
         read cos_bucket_name
-        echo '-e COS_BUCKET_NAME='${cos_bucket_name}' \'
+        echo '-e COS_BUCKET_NAME='${cos_bucket_name}' \' >> ~/blog.sh
 
         echo "请输入COS region:"
         read cos_region
-        echo '-e COS_REGION='${cos_region}' \'
+        echo '-e COS_REGION='${cos_region}' \' >> ~/blog.sh
     fi;
 
     echo '-dit blog:1.0`' >> ~/blog.sh
